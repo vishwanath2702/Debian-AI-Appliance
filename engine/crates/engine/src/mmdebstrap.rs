e7ffe07fa4078b06aaf85cb1d9b97dcaaa6e921e
vishwanath2702 <2nuts4bolts@gmail.com>
2026-07-29 17:33:32 +0530

    refactor(engine): make mmdebstrap bootstrapper testable

--- engine/crates/engine/src/mmdebstrap.rs
+++ engine/crates/engine/src/mmdebstrap.rs
@@ -2,12 +2,26 @@
 
 use std::{
     error::Error,
-    fmt::{self, Display, Formatter},
+    fmt::{self, Debug, Display, Formatter},
     io,
     process::{Command, ExitStatus},
+    sync::Arc,
 };
 
 use crate::{Bootstrapper, BuildContext};
+
+trait CommandRunner: Send + Sync {
+    fn status(&self, command: &mut Command) -> io::Result<ExitStatus>;
+}
+
+#[derive(Clone, Copy, Debug, Default)]
+struct ProcessCommandRunner;
+
+impl CommandRunner for ProcessCommandRunner {
+    fn status(&self, command: &mut Command) -> io::Result<ExitStatus> {
+        command.status()
+    }
+}
 
 /// Error returned when mmdebstrap cannot create a root filesystem.
 #[derive(Debug)]
@@ -40,14 +54,39 @@ impl Error for MmdebstrapError {
 }
 
 /// Constructs commands for bootstrapping Debian root filesystems with mmdebstrap.
-#[derive(Clone, Debug, Default, Eq, PartialEq)]
-pub struct MmdebstrapBootstrapper;
+#[derive(Clone)]
+pub struct MmdebstrapBootstrapper {
+    runner: Arc<dyn CommandRunner>,
+}
+
+impl Debug for MmdebstrapBootstrapper {
+    fn fmt(&self, formatter: &mut Formatter<'_>) -> fmt::Result {
+        formatter
+            .debug_struct("MmdebstrapBootstrapper")
+            .finish_non_exhaustive()
+    }
+}
+
+impl Default for MmdebstrapBootstrapper {
+    fn default() -> Self {
+        Self::new()
+    }
+}
 
 impl MmdebstrapBootstrapper {
     /// Creates an mmdebstrap bootstrapper.
     #[must_use]
-    pub const fn new() -> Self {
-        Self
+    pub fn new() -> Self {
+        Self {
+            runner: Arc::new(ProcessCommandRunner),
+        }
+    }
+
+    #[cfg(test)]
+    fn with_runner(runner: impl CommandRunner + 'static) -> Self {
+        Self {
+            runner: Arc::new(runner),
+        }
     }
 
     /// Constructs the mmdebstrap command for a build context.
@@ -72,9 +111,10 @@ impl Bootstrapper for MmdebstrapBootstrapper {
     type Error = MmdebstrapError;
 
     fn bootstrap(&self, context: &BuildContext) -> Result<(), Self::Error> {
+        let mut command = self.command(context);
         let status = self
-            .command(context)
-            .status()
+            .runner
+            .status(&mut command)
             .map_err(MmdebstrapError::Process)?;
 
         if status.success() {
@@ -87,13 +127,40 @@ impl Bootstrapper for MmdebstrapBootstrapper {
 
 #[cfg(test)]
 mod tests {
-    use std::ffi::OsStr;
+    use std::{
+        ffi::OsStr,
+        io,
+        os::unix::process::ExitStatusExt,
+        process::{Command, ExitStatus},
+        sync::Mutex,
+    };
 
-    use super::MmdebstrapBootstrapper;
-    use crate::{BootstrapConfig, BuildContext};
+    use super::{CommandRunner, MmdebstrapBootstrapper, MmdebstrapError};
+    use crate::{BootstrapConfig, Bootstrapper, BuildContext};
 
-    #[test]
-    fn constructs_mmdebstrap_command() {
+    struct RecordingCommandRunner {
+        result: Mutex<Option<io::Result<ExitStatus>>>,
+    }
+
+    impl RecordingCommandRunner {
+        fn returning(result: io::Result<ExitStatus>) -> Self {
+            Self {
+                result: Mutex::new(Some(result)),
+            }
+        }
+    }
+
+    impl CommandRunner for RecordingCommandRunner {
+        fn status(&self, _command: &mut Command) -> io::Result<ExitStatus> {
+            self.result
+                .lock()
+                .unwrap()
+                .take()
+                .expect("recording runner should only be invoked once")
+        }
+    }
+
+    fn test_context() -> BuildContext {
         let config = BootstrapConfig::new(
             "bookworm",
             "amd64",
@@ -102,13 +169,18 @@ mod tests {
             "minbase",
         );
 
-        let context = BuildContext::new(
+        BuildContext::new(
             "build/rootfs",
             "images/source.iso",
             "build/work",
             "build/output.iso",
             config,
-        );
+        )
+    }
+
+    #[test]
+    fn constructs_mmdebstrap_command() {
+        let context = test_context();
 
         let command = MmdebstrapBootstrapper::new().command(&context);
         let arguments = command.get_args().collect::<Vec<_>>();
@@ -125,5 +197,34 @@ mod tests {
                 OsStr::new("https://deb.debian.org/debian"),
             ]
         );
+    }
+
+    #[test]
+    fn bootstrap_succeeds_when_command_succeeds() {
+        let runner = RecordingCommandRunner::returning(Ok(ExitStatus::from_raw(0)));
+        let bootstrapper = MmdebstrapBootstrapper::with_runner(runner);
+
+        bootstrapper.bootstrap(&test_context()).unwrap();
+    }
+
+    #[test]
+    fn bootstrap_returns_process_error_when_command_cannot_run() {
+        let runner =
+            RecordingCommandRunner::returning(Err(io::Error::other("process unavailable")));
+        let bootstrapper = MmdebstrapBootstrapper::with_runner(runner);
+
+        let error = bootstrapper.bootstrap(&test_context()).unwrap_err();
+
+        assert!(matches!(error, MmdebstrapError::Process(_)));
+    }
+
+    #[test]
+    fn bootstrap_returns_error_when_command_exits_unsuccessfully() {
+        let runner = RecordingCommandRunner::returning(Ok(ExitStatus::from_raw(1 << 8)));
+        let bootstrapper = MmdebstrapBootstrapper::with_runner(runner);
+
+        let error = bootstrapper.bootstrap(&test_context()).unwrap_err();
+
+        assert!(matches!(error, MmdebstrapError::Unsuccessful(_)));
     }
 }
