61ab9d798c5690daff3f3ea5d155d40fb35247ba
vishwanath2702 <2nuts4bolts@gmail.com>
2026-07-31 05:35:09 +0530

    0062 Test whitespace-only ISO media type

--- engine/crates/engine/src/backend/iso/stage.rs
+++ engine/crates/engine/src/backend/iso/stage.rs
@@ -788,6 +788,25 @@ mod tests {
         assert_eq!(error.kind(), std::io::ErrorKind::InvalidData);
     }
     #[test]
+    fn rejects_whitespace_only_media_type() {
+        let source_iso = PathBuf::from("debian.iso");
+        let metadata = IsoMetadata::new(
+            source_iso.clone(),
+            "Debian".to_owned(),
+            "13.1.0".to_owned(),
+            "trixie".to_owned(),
+            "amd64".to_owned(),
+            "   ".to_owned(),
+            vec![BootMode::Bios, BootMode::Uefi],
+        );
+        let context = iso_context(&source_iso, Some(metadata));
+
+        let error = MetadataValidationStage::run(&context)
+            .expect_err("whitespace-only media type should be rejected");
+
+        assert_eq!(error.kind(), std::io::ErrorKind::InvalidData);
+    }
+    #[test]
     fn rejects_empty_boot_modes() {
         let source_iso = PathBuf::from("debian.iso");
         let metadata = IsoMetadata::new(
